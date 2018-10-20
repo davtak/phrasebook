00c24f98fbf2f21d8f0d40c64c1e37496c143dc9
Christian Meredith <christian.meredith@outlook.com.au>
2018-10-20 17:25:10 +1030

    mkdir `-p` after dir creates new folder in some bash/zsh implementations

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Here we go!"
-mkdir ./target/app/ -p
+mkdir -p ./target/app/
 cp ./src/main/css/*.css ./target/app/
 cp ./src/main/js/*.js ./target/app/
 cp ./src/main/html/*.html ./target/app/
